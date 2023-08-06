007a9cb50c91ccc0d6c613c36879d8a340e6779f
cavazani <gabriel_cavazani@hotmail.com>
2023-08-06 00:03:13 -0300

    Alterando exibição do titulo nas funções RegistrarBanda e MostrarBandasRegitradas

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -69,9 +69,7 @@ void ExibirOpcoesDoMenu()
 void RegistrarBanda()
 {
     Console.Clear();
-    Console.WriteLine("*******************");
-    Console.WriteLine("Registro de bandas");
-    Console.WriteLine("*******************\n");
+    ExibirTituloDaOpcao("Registro de bandas");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
     listaDasBandas.Add(nomeDaBanda); //.Add Adiciona o nomeDaBanda na lista de string vazia
@@ -84,9 +82,7 @@ void RegistrarBanda()
 void MostrarBandasRegistradas()
 {
     Console.Clear();
-    Console.WriteLine("************************************");
-    Console.WriteLine("Exibindo todas as bandas registradas");
-    Console.WriteLine("************************************\n");
+    ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
 
     //Utilizando FOR Exemplo
     //for(int i = 0; i < listaDasBandas.Count; i++)
@@ -103,6 +99,15 @@ void MostrarBandasRegistradas()
     Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos);
 }
 
 
