d558288339b5967008b9188afe64ace7f2ea01d1
cavazani <gabriel_cavazani@hotmail.com>
2023-08-08 17:15:51 -0300

    Atribuindo valores direto na propriedade somente para leitura

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -8,6 +8,20 @@ class Musica
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
 
+    //Modelo construindo o valor que quer atribuir a essa propriedade só GET
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+    //exemplo da estrutura acima menos utilizada
+    //public string DescricaoResumida // Utilizar somente leitura GET
+    //{
+    //    get 
+    //    {
+    //        return $"A música {Nome} pertence a banda {Artista}";
+    //    }
+    
+    //}
+
+
     //Podemos criar comportamentos para classe, metodos
 
     //Exemplo Padrao antigo Escrita - Metodo void não retorna nada mas outros metodos de outras classes tera acesso
